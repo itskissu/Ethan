2837e902790c7e6d91f0fdedcdde795e77bba630
Ethankelin8 <hfhfjff743@gmail.com>
2024-09-20 09:43:38 +0600

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -4,7 +4,7 @@ const bot = new Telegraf(TOKEN);
 const express = require("express");
 const app = express()
 app.use(express.json())
-const web_link = "https://gojotap.netlify.app";
+const web_link = "https://suroviik.vercel.app";
 const community_link = "https://t.me/MR_ETHAN_KELIN";
 
 
